39ba7f59d8d2597d8eb6491a1240550b927871db
Janek Priimann <janek.priimann@gmail.com>
2012-10-17 18:29:09 +0300

    Add connection pool support

--- src/saffron/MySQL.hx
+++ src/saffron/MySQL.hx
@@ -15,12 +15,29 @@ typedef MySQLOptions = {
     var database : String;
 }
 
+typedef MySQLPoolOptions = { > MySQLOptions,
+    var poolSize : Int;
+}
+
 extern class MySQL {
     public static function createConnection(options : MySQLOptions) : DataAdapter;
+    public static function createConnectionFromPool(options : MySQLPoolOptions) : DataAdapter;
     
     private static function __init__() : Void untyped {
         try {
             saffron.MySQL = Node.require("mysql");
+            saffron.MySQL.reusablePools = { };
+            saffron.MySQL.createConnectionFromPool = function(options) {
+                var key = '' + options.host + ':' + options.port + ':' + options.user + ':' + options.password + ':' + options.database;
+                var pool = saffron.MySQL.reusablePools[key];
+                
+                if(pool == null) {
+                    pool = __js__("new require('mysql-pool').MySQLPool(options)");
+                    saffron.MySQL.reusablePools[key] = pool;
+                }
+                
+                return pool;
+            };
         }
         catch(e : Dynamic) {
         }
