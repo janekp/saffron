1d714c4544ce4df8b123686e80e45b4946456291
Janek Priimann <janek.priimann@gmail.com>
2012-10-17 12:07:58 +0300

    Improve DataResult class def

--- src/saffron/Data.hx
+++ src/saffron/Data.hx
@@ -15,15 +15,19 @@ typedef DataError = {
     var fatal : Bool;
 }
 
-@:native('Array') extern class DataResult {
+@:native('Array') extern class DataResult implements ArrayAccess<Dynamic> {
     var insertId : DataIdentifier;
     var length : Int;
     
     public function new() : Void;
     public function push(item : Dynamic) : Void;
     
-    public inline function get(index : Int) : Dynamic {
+    public inline function row(index : Int) : Dynamic {
         return untyped this[index];
+    }
+    
+    public inline function rows() : Array<Dynamic> {
+        return untyped this;
     }
 }
 
