ce595f8c3ea576b85371bba744f699fc0a5a6f0f
Janek Priimann <janek.priimann@gmail.com>
2012-10-17 11:42:23 +0300

    Update the Data API once again

--- examples/HelloWorld/src/ItemPage.hx
+++ examples/HelloWorld/src/ItemPage.hx
@@ -7,7 +7,7 @@ import saffron.Page;
 class ItemPage extends Page {
     public function index(id : Int) : Void {
         this.query('SELECT * FROM ITEMS WHERE id = ?', [ id ], function(err, results) {
-            this.render((results.length == 1) ? results[0] : null);
+            this.render((results.length == 1) ? results.get(0) : null);
         });
     }
 }
--- examples/HelloWorld/src/MockDatabase.hx
+++ examples/HelloWorld/src/MockDatabase.hx
@@ -14,7 +14,7 @@ typedef MockDatabaseItem = {
 class MockDatabase {
     private static var shared : MockDatabase = null;
     
-    private var data : Array<MockDatabaseItem>;
+    private var data : DataResult;
     
     public static function adapter() : DataAdapter {
         if(shared == null) {
@@ -25,28 +25,22 @@ class MockDatabase {
     }
     
     private function new() {
-        this.data = new Array<MockDatabaseItem>();
+        this.data = new DataResult();
         this.data.push({ id: 1, title: 'Hello #1', description: 'Hello World!' });
         this.data.push({ id: 2, title: 'Hello #2', description: 'Hello World!!' });
         this.data.push({ id: 3, title: 'Hello #3', description: 'Hello World!!!' });
     }
     
-    public function exec(q : String, ?p : Array<Dynamic>, fn : DataError -> DataResult -> Void) : Void {
-        Environment.setTimeout(function() {
-            fn({ code: 'Unsupported', fatal: true }, null);
-        }, 10);
-    }
-    
-    public function query(q : String, ?p : Array<Dynamic>, fn : DataError -> Array<Dynamic> -> Void) : Void {
+    public function query(q : String, ?p : Array<Dynamic>, fn : DataError -> DataResult -> Void) : Void {
         Environment.setTimeout(function() {
             if(q == 'SELECT * FROM ITEMS') {
                 fn(null, this.data);
             } else if(q == 'SELECT * FROM ITEMS WHERE id = ?' && p != null && p.length == 1) {
-                var result : Array<Dynamic> = new Array<Dynamic>();
+                var result = new DataResult();
                 
                 for(i in 0...this.data.length) {
-                    if(p[0] == this.data[i].id) {
-                        result.push(this.data[i]);
+                    if(p[0] == this.data.get(i).id) {
+                        result.push(this.data.get(i));
                         break;
                     }
                 }
