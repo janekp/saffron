2e216057f767d6b9444cc6f2487e8beb7bbc1354
Janek Priimann <janek.priimann@gmail.com>
2013-03-13 00:54:12 +0200

    Add basic locale hook

---
+++ src/saffron/Locale.hx
@@ -0,0 +1,30 @@
+/* Copyright (c) 2013 Janek Priimann */
+
+package saffron;
+
+class Locale {
+    private static var strings : Dynamic = { };
+    private static var code : String = null;
+    
+    public static function register(code : String, strings : Dynamic) : Void {
+        if(Locale.code == null) {
+            Locale.code = code;
+        }
+        
+        Locale.strings[untyped code] = untyped strings;
+    }
+    
+    public static function get(key : String) : String {
+        var value = null;
+        
+        if(key != null) {
+            value = (Locale.code != null) ? Locale.strings[untyped Locale.code][untyped key] : null;
+            
+            if(value == null && Locale.strings.en != null) {
+                value = Locale.strings.en[untyped key];
+            }
+        }
+        
+        return (value != null) ? value : key;
+    }
+}
