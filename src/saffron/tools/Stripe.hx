7279af23403d722c18ffa624d3be874a08216f1f
Janek Priimann <janek.priimann@gmail.com>
2013-10-25 01:49:52 +0300

    Update Stripe dep

--- src/saffron/tools/Stripe.hx
+++ src/saffron/tools/Stripe.hx
@@ -15,6 +15,27 @@ typedef StripeChargesCreateOptions = {
 	?application_fee : Int
 };
 
+typedef StripeCharge = {
+	var id : String;
+	var object : String;
+	var created : Int;
+	var livemode : Bool;
+	var paid : Bool;
+	var amont : Int;
+	var currency : String;
+	var refunded : Bool;
+	var card : Dynamic;
+	var captured : Bool;
+	var balance_transaction : String;
+	var failure_message : String;
+	var failure_code : Int;
+	var amount_refunded : Int;
+	var customer : Dynamic;
+	var invoice : Dynamic;
+	var description : String;
+	var metadata : Dynamic;
+};
+
 typedef StripePromise = {
 	public function then(ok_fn : Dynamic -> Void, error_fn : Dynamic -> Void) : StripePromise;
 };
