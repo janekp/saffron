3978dbee699fccae09c6fd9d40065240783ac678
Janek Priimann <janek.priimann@gmail.com>
2013-10-25 22:54:20 +0300

    Update Stripe dep

--- src/saffron/tools/Stripe.hx
+++ src/saffron/tools/Stripe.hx
@@ -30,13 +30,42 @@ typedef StripeCharge = {
 	var failure_message : String;
 	var failure_code : Int;
 	var amount_refunded : Int;
-	var customer : Dynamic;
+	var customer : String;
 	var invoice : Dynamic;
 	var description : String;
 	var metadata : Dynamic;
 };
 
+typedef StripeCustomersCreateOptions = {
+	?account_balance : Int,
+	?card : Dynamic,
+	?coupon : Dynamic,
+	?description : String,
+	?email : String,
+	?metadata : Dynamic,
+	?plan : Dynamic,
+	?quantity : Int,
+	?trial_end : Int
+};
+
+typedef StripeCustomer = {
+	var id : String;
+	var object : String;
+	var created : Int;
+	var livemode : Bool;
+	var description : String;
+	var email : String;
+	var delinquent : Bool;
+	var metadata : Dynamic;
+	var subscription : Dynamic;
+	var discount : Dynamic;
+	var account_balance : Int;
+	var cards : Array<Dynamic>;
+	var default_card : Dynamic;
+};
+
 typedef StripePromise = {
+	@:overload(function(ok_fn : Dynamic -> Void) : StripePromise {})
 	public function then(ok_fn : Dynamic -> Void, error_fn : Dynamic -> Void) : StripePromise;
 };
 
@@ -44,10 +73,15 @@ typedef StripeCharges = {
 	public function create(options : StripeChargesCreateOptions) : StripePromise;
 };
 
+typedef StripeCustomers = {
+	public function create(options : StripeCustomersCreateOptions) : StripePromise;
+};
+
 extern class Stripe {
 	public static function create(key : String) : Stripe;
 	
 	public var charges : StripeCharges;
+	public var customers : StripeCustomers;
 	
     private static function __init__() : Void untyped {
         if(saffron.tools == null) {
